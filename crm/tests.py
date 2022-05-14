5ba67369cd8ca82521fccdac1a606cd15251c5d4
Mohsen Shafiey Kasrineh <mshafiey@users.noreply.github.com>
2022-05-14 12:29:28 +0300

    add a simle test

--- crm/tests.py
+++ crm/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from .views import Customer ,  orders
 
-# Create your tests here.
+
+
+
+class HomePageTest(TestCase):
+    def test_environment_set_in_context(self):
+        request = RequestFactory().get('/')
+        view = Customer ()
+        view.setup(request)
+
+        context = view.get_context_data()
+        self.assertIn('environment', context)
+
+class ordersPageTest(TestCase):
+    def test_environment_set_in_context(self):
+        request = RequestFactory().get('/')
+        view =  orders()
+        view.setup(request)
+
+        context = view.get_context_data()
+        self.assertIn('environment', context)        
